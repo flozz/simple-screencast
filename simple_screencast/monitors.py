7bc9746a5bbc6e000e6b226bf726eae67b2ef91f
Fabien LOISON <flo@flogisoft.com>
2017-05-13 21:59:58 +0200

    fixes wrong matching between outputs and crtcs

--- simple_screencast/monitors.py
+++ simple_screencast/monitors.py
@@ -13,21 +13,23 @@ class Monitors:
     def list_monitors(self):
         serial, crtcs, outputs, modes, max_screen_width, max_screen_height = self._displayconfig.GetResources()
 
-        for crtc, output in zip(crtcs, outputs):
-            id_, _, x, y, width, height, _, _, _, _ = crtc
-            _, _, _, _, _, _, _, rr_output = output
-            yield {
-                "id": id_,
-                "x": x,
-                "y": y,
-                "width": width,
-                "height": height,
-                "display-name": rr_output["display-name"],
-                "connector-type": rr_output["connector-type"],
-                "height-mm": rr_output["height-mm"],
-                "width-mm": rr_output["width-mm"],
-                "primary": rr_output["primary"],
-                "product": rr_output["product"],
-                "serial": rr_output["serial"],
-                "vendor": rr_output["vendor"]}
+        for _, _, id_, _, _, _, _, rr_output in outputs:
+            for crtcid, _, x, y, width, height, _, _, _, _ in crtcs:
+                if crtcid != id_:
+                    continue
+                yield {
+                    "id": id_,
+                    "x": x,
+                    "y": y,
+                    "width": width,
+                    "height": height,
+                    "display-name": rr_output["display-name"],
+                    "connector-type": rr_output["connector-type"],
+                    "height-mm": rr_output["height-mm"],
+                    "width-mm": rr_output["width-mm"],
+                    "primary": rr_output["primary"],
+                    "product": rr_output["product"],
+                    "serial": rr_output["serial"],
+                    "vendor": rr_output["vendor"]}
+                break
 
