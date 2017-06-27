412647b1efa1aa2359fb714d0815d834281b46a7
Fabien LOISON <flo@flogisoft.com>
2017-06-27 19:32:19 +0200

    pydbus replaced by PyGI/GDbus

--- simple_screencast/screencast.py
+++ simple_screencast/screencast.py
@@ -1,9 +1,4 @@
-from gi.repository import GLib
-import pydbus
-
-
-GNOME_SHELL_SCREENSHOT = "org.gnome.Shell.Screenshot"
-GNOME_SHELL_SCREENCAST = "org.gnome.Shell.Screencast"
+from gi.repository import GLib, Gio
 
 
 class Screencast:
@@ -15,9 +10,23 @@ class Screencast:
             "framerate": 30,
             "pipeline": None
         }
-        self._bus = pydbus.SessionBus()
-        self._screenshot = self._bus.get(GNOME_SHELL_SCREENSHOT)
-        self._screencast = self._bus.get(GNOME_SHELL_SCREENCAST)
+        self._bus = Gio.bus_get_sync(Gio.BusType.SESSION, None)
+        self._screenshot_bus_proxy = Gio.DBusProxy.new_sync(
+                self._bus,
+                Gio.DBusProxyFlags.NONE,
+                None,
+                "org.gnome.Shell.Screenshot",
+                "/org/gnome/Shell/Screenshot",
+                "org.gnome.Shell.Screenshot",
+                None)
+        self._screencast_bus_proxy = Gio.DBusProxy.new_sync(
+                self._bus,
+                Gio.DBusProxyFlags.NONE,
+                None,
+                "org.gnome.Shell.Screencast",
+                "/org/gnome/Shell/Screencast",
+                "org.gnome.Shell.Screencast",
+                None)
 
     def get_options(self):
         return self._options
@@ -30,7 +39,13 @@ class Screencast:
 
     def select_area(self):
         try:
-            return self._screenshot.SelectArea()
+            area = self._screenshot_bus_proxy.call_sync(
+                    "SelectArea",
+                    None,
+                    Gio.DBusCallFlags.NONE,
+                    -1,
+                    None)
+            return area.unpack()
         except GLib.Error, e:
             # Error code 19: Operation was cancelled
             if e.code != 19:
@@ -38,15 +53,42 @@ class Screencast:
             return None
 
     def record_desktop(self):
-        return self._screencast.Screencast(self._options["file-template"], {})
+        response = self._screencast_bus_proxy.call_sync(
+                    "Screencast",
+                    GLib.Variant.new_tuple(
+                        GLib.Variant.new_string(self._options["file-template"]),
+                        GLib.Variant("a{sv}", {})
+                        ),
+                    Gio.DBusCallFlags.NONE,
+                    -1,
+                    None)
+        return response.unpack()
 
     def record_screen(self, screen_id):
         raise NotImplementedError()  # TODO
 
     def record_area(self, x, y, width, height):
-        return self._screencast.ScreencastArea(x, y, width, height,
-                self._options["file-template"], {})
+        response = self._screencast_bus_proxy.call_sync(
+                    "ScreencastArea",
+                    GLib.Variant.new_tuple(
+                        GLib.Variant.new_int32(x),
+                        GLib.Variant.new_int32(y),
+                        GLib.Variant.new_int32(width),
+                        GLib.Variant.new_int32(height),
+                        GLib.Variant.new_string(self._options["file-template"]),
+                        GLib.Variant("a{sv}", {})
+                        ),
+                    Gio.DBusCallFlags.NONE,
+                    -1,
+                    None)
+        return response.unpack()
 
     def stop_recording(self):
-        return self._screencast.StopScreencast()
+        success = self._screencast_bus_proxy.call_sync(
+                    "StopScreencast",
+                    None,
+                    Gio.DBusCallFlags.NONE,
+                    -1,
+                    None)
+        return success.unpack()
 
